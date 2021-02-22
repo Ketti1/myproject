021c922214f1f9664549a1c52c878521b2a06fb3
Ketti1 <74548835+Ketti1@users.noreply.github.com>
2021-02-22 17:54:55 +0200

    Add files via upload

---
+++ les32/mine.js
@@ -0,0 +1,36 @@
+// let x = " " + 1 + 0
+// console.log(x)
+// let y = true + false 
+// console.log(y)
+// let z =  6 / "3"
+// console.log(z)
+// let c = 4 + 5 + "px"
+// console.log(c)
+// let b = "px" + 5 + 4
+// console.log(b)
+// let m = "4" - 2
+// console.log(m)
+// let n = (prompt("Введите первое число"))
+// console.log(n)
+// let o = (prompt("Введите второе число"))
+// console.log(o)
+// let l = 5 > 4 
+// console.log(l)
+// let k = "ананас">"яблуко"
+// console.log(k)
+// let v = "2">"12"
+// console.log(v)
+// let w = "Roma"<"Romanuch"
+// console.log(w)
+let a = prompt("В каком году был опубликован первый сценарий JS?")
+if ( a==2015) {
+    alert("Ты молодец!")
+}
+else if (a > 2015) { 
+    alert("Немного меньше")
+
+}
+else if ( a < 2015) {
+    alert("Немного больше")
+}
+    
